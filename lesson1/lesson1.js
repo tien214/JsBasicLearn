6b6d514e9cadac96ac39f584e2da9ff62827d8a5
tien214 <phanthanhtien21124@gmail.com>
2023-01-07 21:45:25 +0700

    scource lesson4

--- lesson1/lesson1.js
+++
@@ -1,1 +0,0 @@
-alert('Hello Meo!')//Con mua ngan ua
--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -1,2 +1,9 @@
 let a = 'Toi la mot string - chuoi'
-console.log('')
+let x = 'x'  
+console.log(a)
+
+console.log(x.length + 5 + 4)
+
+console.log(4 + x.length + 5)
+
+console.log(4 + 5 + x.length)
